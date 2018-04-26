274a844197e5d8b1e9ffd84b8ebf98a8887b934b
Lindsey Hodel <lindsey.c.hodel.19@dartmouth.edu>
2018-04-26 15:28:07 -0400

    added pics

--- src/components/CarouselComp.js
+++ src/components/CarouselComp.js
@@ -9,10 +9,10 @@ function onChange(a, b, c) {
 const CarouselComp = (props) => {
   return (
     <Carousel afterChange={onChange}>
-      <div><img alt="sky" src="../src/img/sky.jpg" /></div>
       <div><img alt="mountain" src="../src/img/mountains.jpg" /></div>
       <div><img alt="road" src="../src/img/road.jpg" /></div>
       <div><img alt="sea" src="../src/img/seaPic.jpg" /></div>
+      <div><img alt="sky" src="../src/img/sky.jpg" /></div>
     </Carousel>
   );
 };
